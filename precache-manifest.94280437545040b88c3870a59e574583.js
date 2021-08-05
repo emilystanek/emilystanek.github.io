7f627168f2dc7a45676c17acbfd7e8a762e8e432
Emily Stanek <estanek@ackroo.com>
2021-08-05 15:14:39 -0400

    Updates

---
+++ precache-manifest.94280437545040b88c3870a59e574583.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad87b358043859bfe67f7543ecc38d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20bcb10e587d4d818479",
+    "url": "/static/css/2.f138c75b.chunk.css"
+  },
+  {
+    "revision": "de9f37666387b2b010dd",
+    "url": "/static/css/main.3a707b74.chunk.css"
+  },
+  {
+    "revision": "20bcb10e587d4d818479",
+    "url": "/static/js/2.f58f4089.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.f58f4089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de9f37666387b2b010dd",
+    "url": "/static/js/main.1fc215dd.chunk.js"
+  },
+  {
+    "revision": "b4a590ae1b507e5de256",
+    "url": "/static/js/runtime-main.9f96186b.js"
+  },
+  {
+    "revision": "cb5565dc01c8918bb63ce25414b9e5e4",
+    "url": "/static/media/me.cb5565dc.png"
+  }
+]);
