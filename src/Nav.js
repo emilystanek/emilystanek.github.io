15bfdcd3dcd42da5da7f7cce98ab8731da440bcc
Emily Stanek <stanekemily@gmail.com>
2022-02-13 00:14:40 -0500

    Mobile styling

--- src/Nav.js
+++ src/Nav.js
@@ -7,7 +7,7 @@ const Nav = () => {
             <header className="navbar navbar-expand-md navbar-floating navbar-sticky navbar-dark" data-fixed-element="">
                 <div className="container">
                     <a className="navbar-brand" href="index.html">
-                        <img className=" navbar-floating-logo d-none d-lg-block" src={Me} alt="Around" width="100" />
+                        <img className="navbar-floating-logo d-lg-block" src={Me} alt="Around" width="100" />
                     </a>
                     <ul className="navbar-nav">
                         <li className="nav-item">
