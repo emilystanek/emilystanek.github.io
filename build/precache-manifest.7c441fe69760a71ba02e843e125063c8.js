3cdb2a4d9fd18986443f5bd793d9818e658ce471
Emily Stanek <stanekemily@gmail.com>
2023-12-07 13:49:40 -0500

    Revert "simplify"
    This reverts commit 9764ad93e221b6b82702a15e26a34c0272ae6644.

---
+++ build/precache-manifest.7c441fe69760a71ba02e843e125063c8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8388ad7b4fbe4d4ab1eebb7fc5665f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8659a29e63fcff2e9c4f",
+    "url": "/static/css/2.c3097103.chunk.css"
+  },
+  {
+    "revision": "2bd6207093fd47f655ba",
+    "url": "/static/css/main.98a03f2b.chunk.css"
+  },
+  {
+    "revision": "8659a29e63fcff2e9c4f",
+    "url": "/static/js/2.04413a17.chunk.js"
+  },
+  {
+    "revision": "3ef937a2d09454cf26cc6d861c4da352",
+    "url": "/static/js/2.04413a17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bd6207093fd47f655ba",
+    "url": "/static/js/main.2f9d0a34.chunk.js"
+  },
+  {
+    "revision": "b4a590ae1b507e5de256",
+    "url": "/static/js/runtime-main.9f96186b.js"
+  },
+  {
+    "revision": "32c16a6534f0c8e5bbcb949e0d4c1f89",
+    "url": "/static/media/computer.32c16a65.jpg"
+  },
+  {
+    "revision": "1b4760f2d87aa0386444b5b526288492",
+    "url": "/static/media/me.1b4760f2.png"
+  }
+]);
